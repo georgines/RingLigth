8418687f9e30d419bfecbc036416247e0e7e16c0
georgines <geoginae.p2@gmail.com>
2022-04-24 17:16:47 -0300

    adicionando configuracoes do ambiente esp32

--- src/main.cpp
+++ src/main.cpp
@@ -91,13 +91,6 @@ void setup()
     BotaoTrocaCores.criar();
     LuzBranca.criar();
     LuzAmarela.criar();
-
-    // pinMode(3, OUTPUT);
-    // pinMode(11, OUTPUT);
-    // TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-    // TCCR2B = _BV(CS22);
-    // OCR2A = 180;
-    // OCR2B = 50;
 }
 
 void loop()
